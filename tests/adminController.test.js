dc00e12babdc5c5a11ff236c806b88b8263373ac
Jigsawsam <bgt117@gmail.com>
2025-08-12 14:12:48 -0700

    jest testing

--- tests/adminController.test.js
+++ tests/adminController.test.js
@@ -65,7 +65,6 @@ describe('adminController', () => {
       expect(getDatabase().collection).toHaveBeenCalledWith('users');
       expect(mockCollection.findOne).toHaveBeenCalledWith({
         _id: new ObjectId(req.params.id),
-        role: 'admin'
       });
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith(admin);
